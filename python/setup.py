9a753bb971271ed27e21778ef6cd2d3c2b57b4f8
li-lu <li_lu2000@hotmail.com>
2017-11-30 09:58:08 -0500

    Initial Loading

---
+++ python/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='ads',
+    version='0.1',
+    description='ads',
+    url='https://github.com/li-lu/bd',
+    author='li-lu',
+    author_email='li_lu2000@hotmail.com',
+    license='MIT',
+    packages=['ads'],
+    install_requires=[
+        'markdown',
+    ],
+    zip_safe=False)
