d900585d152528f6e057f0729c593654cba9821a
llu <llu@ATL1L-837J7H2.corp.etradegrp.com>
2018-01-02 11:20:00 -0500

    Test.

--- ws/src/main/java/ws/Hello.java
+++ ws/src/main/java/ws/Hello.java
@@ -13,7 +13,7 @@ public class Hello {
 	@Produces("text/html")
 	public Response getStartingPage()
 	{
-		String output = "<h1>Hello World!<h1>" +
+		String output = "<h1>Hello Data Scientist!<h1>" +
 				"<p>RESTful Service is running ... <br>Ping @ " + new Date().toString() + "</p<br>";
 		return Response.status(200).entity(output).build();
 	}
